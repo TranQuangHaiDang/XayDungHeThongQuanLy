b2330d8de00489fd0a9e86dacff87a0a75e83faa
TranQuangHaiDang <93525656+TranQuangHaiDang@users.noreply.github.com>
2021-12-03 10:50:47 +0700

    Add files via upload

---
+++ CuuSinhVien.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaiTapCuoiKi.People
+{
+    class CuuSinhVien : SinhVien
+    {
+        string namTotNghiep;
+        public string NamTotNghiep
+        {
+            get => namTotNghiep;
+            set => namTotNghiep = value;
+        }
+        public CuuSinhVien(string namTotNghiep, string namNhapHoc, string khoaHoc, string khoa,
+                           int maSoSinhVien, string nganhHoc, string lopSinhVien, int diemRenLuyen,
+                           int diemCTXH, string ho, string ten, string ngaySinh, string gioiTinh,
+                           string diaChi, string soDT)
+        {
+            this.namTotNghiep = namTotNghiep;
+            NamNhapHoc = namNhapHoc;
+            KhoaHoc = khoaHoc;
+            Khoa = khoa;
+            MaSoSinhVien = maSoSinhVien;
+            NganhHoc = nganhHoc;
+            LopSinhVien = lopSinhVien;
+            DiemRenLuyen = diemRenLuyen;
+            DiemCTXH = diemCTXH;
+            Ho = ho;
+            Ten = ten;
+            NgaySinh = ngaySinh;
+            GioiTinh = gioiTinh;
+            DiaChi = diaChi;
+            SoDT = soDT;
+        }
+
+        public CuuSinhVien(CuuSinhVien cuusinhvien) : this(cuusinhvien.NamNhapHoc, cuusinhvien.namTotNghiep,
+                                                      cuusinhvien.KhoaHoc, cuusinhvien.Khoa, cuusinhvien.MaSoSinhVien,
+                                                      cuusinhvien.NganhHoc, cuusinhvien.LopSinhVien, cuusinhvien.DiemRenLuyen,
+                                                      cuusinhvien.DiemCTXH, cuusinhvien.Ho, cuusinhvien.Ten, cuusinhvien.NgaySinh,
+                                                      cuusinhvien.GioiTinh, cuusinhvien.SoDT, cuusinhvien.DiaChi)
+        {
+        }
+        public CuuSinhVien()
+        {
+            namTotNghiep = "";
+            NamNhapHoc = "";
+            KhoaHoc = "";
+            Khoa = "";
+            MaSoSinhVien = 0;
+            NganhHoc = "";
+            LopSinhVien = "";
+            DiemCTXH = 0;
+            DiemRenLuyen = 0;
+            Ho = "";
+            Ten = "";
+            NgaySinh = "";
+            GioiTinh = "";
+            DiaChi = "";
+            SoDT = "";
+        }
+    }
+}
---
+++ HocSinh.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaiTapCuoiKi.People
+{
+    public class HocSinh : Person
+    {
+        int soBaoDanh;
+        string truongTHPT;
+        string khoiThi;
+        string nganh;
+        int diemThi;
+        int khuVuc;
+        double diemUuTien;
+        string dienTuyenSinh;
+        public int SoBaoDanh
+        {
+            get => soBaoDanh;
+            set => soBaoDanh = value;
+        }
+        public string TruongTHPT
+        {
+            get => truongTHPT;
+            set => truongTHPT = value;
+        }
+        public string KhoiThi
+        {
+            get => khoiThi;
+            set => khoiThi = value;
+        }
+        public string Nganh
+        {
+            get => nganh;
+            set => nganh = value;
+        }
+        public int DiemThi
+        {
+            get => diemThi;
+            set => diemThi = value;
+        }
+        public int KhuVuc
+        {
+            get => khuVuc;
+            set => khuVuc = value;
+        }
+        public double DiemUuTien
+        {
+            get => diemUuTien;
+            set => diemUuTien = value;
+        }
+        public string DienTuyenSinh
+        {
+            get => dienTuyenSinh;
+            set => dienTuyenSinh = value;
+        }
+        public HocSinh(int soBaoDanh, string truongTHPT, string khoiThi, string nganh, int diemThi,
+                       int khuVuc, double diemUuTien, string dienTuyenSinh, string ho, string ten,
+                       string ngaySinh, string gioiTinh, string soDT, string diaChi)
+        {
+            this.soBaoDanh = soBaoDanh;
+            this.truongTHPT = truongTHPT;
+            this.khoiThi = khoiThi;
+            this.nganh = nganh;
+            this.diemThi = diemThi;
+            this.khuVuc = khuVuc;
+            this.diemUuTien = diemUuTien;
+            this.dienTuyenSinh = dienTuyenSinh;
+            Ho = ho;
+            Ten = ten;
+            NgaySinh = ngaySinh;
+            GioiTinh = gioiTinh;
+            SoDT = soDT;
+            DiaChi = diaChi;
+        }
+        public HocSinh(HocSinh hocsinh) : this(hocsinh.soBaoDanh, hocsinh.truongTHPT, hocsinh.khoiThi,
+                                          hocsinh.nganh, hocsinh.diemThi, hocsinh.khuVuc, hocsinh.diemUuTien,
+                                          hocsinh.dienTuyenSinh, hocsinh.Ho, hocsinh.Ten,
+                                          hocsinh.NgaySinh, hocsinh.GioiTinh, hocsinh.SoDT, hocsinh.DiaChi)
+        {
+        }
+        public HocSinh()
+        {
+            soBaoDanh = 0;
+            truongTHPT = "";
+            khoiThi = "";
+            nganh = "";
+            diemThi = 0;
+            khuVuc = 0;
+            diemUuTien = 0;
+            dienTuyenSinh = "";
+            Ho = "";
+            Ten = "";
+            NgaySinh = "";
+            GioiTinh = "";
+            DiaChi = "";
+            SoDT = "";
+        }
+    }
+}
