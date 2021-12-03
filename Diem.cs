3cef4bd5dfd2873fb2b3b7a6be04e271d5273cb6
TranQuangHaiDang <93525656+TranQuangHaiDang@users.noreply.github.com>
2021-12-03 10:52:09 +0700

    Add files via upload

---
+++ Diem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaiTapCuoiKi.Other
+{
+    class Diem
+    {
+        int diemCuoiKi;
+        int diemQuaTrinh;
+        int diemTrungBinh;
+        string diemChu;
+        public int DiemCuoiKi
+        {
+            get => diemCuoiKi;
+            set => diemCuoiKi = value;
+        }
+        public int DiemQuaTrinh
+        {
+            get => diemQuaTrinh;
+            set => diemQuaTrinh = value;
+        }
+        public int DiemTrungBinh
+        {
+            get => diemTrungBinh;
+            set => diemTrungBinh = value;
+        }
+        public string DiemChu
+        {
+            get => diemChu;
+            set => diemChu = value;
+        }
+        public Diem(int diemCuoiKi, int diemQuaTrinh, int diemTrungBinh, string diemChu)
+        {
+            this.diemCuoiKi = diemCuoiKi;
+            this.diemQuaTrinh = diemQuaTrinh;
+            this.diemTrungBinh = diemTrungBinh;
+            this.diemChu = diemChu;
+        }
+        public Diem(Diem diem) : this(diem.diemCuoiKi, diem.diemQuaTrinh, diem.diemTrungBinh, diem.diemChu)
+        {
+        }
+        public Diem()
+        {
+            diemCuoiKi = 0;
+            DiemQuaTrinh = 0;
+            diemTrungBinh = 0;
+            diemChu = "";
+        }
+    }
+}
---
+++ Nganh.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaiTapCuoiKi.Other
+{
+    class Nganh
+    {
+        int maNganh;
+        int tenNganh;
+
+        public int MaNganh { get => maNganh; set => maNganh = value; }
+        public int TenNganh { get => tenNganh; set => tenNganh = value; }
+
+        public Nganh(int maNganh, int tenNganh)
+        {
+            this.maNganh = maNganh;
+            this.tenNganh = tenNganh;
+        }
+
+        public Nganh()
+        {
+        }
+    }
+}
